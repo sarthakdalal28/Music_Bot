1a59756d110a4deb0f27576e2b6d1d5390af54eb
Sarthak Dalal <sarthak.dalal@gmail.com>
2021-02-23 22:11:27 +0000

    Added feature to play songs from youtube link

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 import discord
 import os
 from discord.ext import commands
+import youtube_dl
 
 client = commands.Bot(command_prefix="!")
 @client.command()
 async def play(ctx, url : str):
-  voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Songs')
-  voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
-  if not voice.is_connected():
-    await voiceChannel.connect()
+  song_there = os.path.isfile('song.mp3')
+  try:
+    if song_there:
+      os.remove('song.mp3')
+  except PermissionError:
+    await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+    return
 
+  voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='Songs')
+  await voiceChannel.connect()
+  voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+
+  
+  ydl_opts={
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+      'key': 'FFmpegExtractAudio',
+      'prefferedcodec': 'mp3',
+      'prefferedquality': '192',
+    }],
+  }
+  with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
+  for file in os.listdir("./"):
+    if file.endswith(".mp3"):
+      os.rename(file, "song.mp3")
+  voice.play(discord.FFmpegPCMAudio("song.mp3"))
 @client.command()
 async def leave(ctx):
   voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
