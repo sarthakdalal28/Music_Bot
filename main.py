b3fe8a77629d8e628b1fd253632aac85911a7f25
Sarthak Dalal <sarthak.dalal@gmail.com>
2021-02-23 21:45:29 +0000

    Added commands to leave, pause, resume and stop the bot. Also installed ffmpeg and youtube.dl

--- main.py
+++ main.py
@@ -10,4 +10,32 @@ async def play(ctx, url : str):
   if not voice.is_connected():
     await voiceChannel.connect()
 
+@client.command()
+async def leave(ctx):
+  voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+  if voice.is_connected():
+    await voice.disconnect()
+  else:
+    await ctx.send('The bot is not connected to a voice channel.')
+
+@client.command()
+async def pause(ctx):
+  voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+  if voice.is_playing():
+    voice.pause()
+  else:
+    await ctx.send('Currently no audio is playing.')
+
+@client.command()
+async def resume(ctx):
+  voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+  if voice.is_paused():
+    voice.resume()
+  else:
+    await ctx.send('The audio is already playing.')
+
+@client.command()
+async def stop(ctx):
+  voice = discord.utils.get(client.voice_clients, guild = ctx.guild)
+  voice.stop()
 client.run(os.getenv('TOKEN')) #run the bot
